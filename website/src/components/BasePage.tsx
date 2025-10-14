0aaef407686cf4656d7950bc38cf81160e42ff60
Maxime <dexeat@gmail.com>
2025-10-14 12:18:24 +0200

    💄 style bouton et debut navbar

--- website/src/components/BasePage.tsx
+++ website/src/components/BasePage.tsx
@@ -3,8 +3,19 @@ import { Outlet } from "react-router";
 function BasePage() {
   return (
     <div>
-      <h1>Welcome to the Tournament Report</h1>
-      <p>This is the base page of the application.</p>
+      <nav className="bg-gray-900">
+        <div className="p0 pt-1 pl-1 flex items-center">
+          <ul className="flex space-x-4">
+            {/* ... éléments ... */}
+          </ul>
+
+          <button className="btn-gradient group ml-auto">
+            <span className="btn-gradient-inner">
+              Login
+            </span>
+          </button>
+        </div>
+      </nav>
       <Outlet />
     </div>
   );
--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router"
 import './index.css'
 import BasePage from "./components/BasePage"
+import Home from "./pages/Home"
 
 const root = document.getElementById('root') as HTMLElement
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<BasePage />}>
+        <Route index element={<Home />} />
       </Route>
     </Routes>
   </BrowserRouter>
